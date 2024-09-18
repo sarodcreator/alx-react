2ec8f38841b9201726668286d6b01ec91956e608
Baridoo Saro <sarobaridoo@gmail.com>
2024-09-18 09:57:26 +0000

    fixed

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,21 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const headerStyle = { backgroundColor: '#deb5b545' };
-
 const rowStyle = { backgroundColor: '#f5f5f5ab' };
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
   return (
     <tr style={isHeader ? headerStyle : rowStyle}>
       {isHeader ? (
-        textSecondCell === null ? (
-          <th colSpan={2}>{textFirstCell}</th>
-        ) : (
+        textSecondCell ? (
           <>
             <th>{textFirstCell}</th>
             <th>{textSecondCell}</th>
           </>
+        ) : (
+          <th colSpan="2">{textFirstCell}</th>
         )
       ) : (
         <>
@@ -30,7 +29,7 @@ function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
   textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  textSecondCell: PropTypes.string,
 };
 
 CourseListRow.defaultProps = {
@@ -39,3 +38,4 @@ CourseListRow.defaultProps = {
 };
 
 export default CourseListRow;
+
