1b1818448240556d099a6a1e9b6132a1af503275
Saro <sarobaridoo@gmail.com>
2024-10-09 16:50:25 +0100

    fixed

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.test.js
@@ -1,1 +1,29 @@
+import { fromJS } from 'immutable';
+import courseReducer from '../reducers/courseReducer';
+import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActions';
 
+describe('courseReducer', () => {
+    it('should handle FETCH_COURSE_SUCCESS', () => {
+        const initialState = fromJS({
+            courses: {},
+        });
+        const action = {
+            type: FETCH_COURSE_SUCCESS,
+            data: [/* mock course data */],
+        };
+        const newState = courseReducer(initialState, action);
+        // Add assertions here to check the normalized state
+    });
+
+    it('should handle SELECT_COURSE', () => {
+        const initialState = fromJS({
+            courses: {
+                1: { selected: false },
+            },
+        });
+        const action = { type: SELECT_COURSE, courseId: 1 };
+        const newState = courseReducer(initialState, action);
+        expect(newState.getIn(['courses', 1, 'selected'])).toBe(true);
+    });
+
+});
